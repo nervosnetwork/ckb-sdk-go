ef4a49d5ace3cf15a03d5987496cd36d68194e4c
shaojunda <shaojunda@gmail.com>
2020-11-25 15:11:37 +0800

    chore: use LiveCellCollector on payment

--- payment/payment.go
+++ payment/payment.go
@@ -14,18 +14,16 @@ import (
 )
 
 type Payment struct {
-	From            *types.Script
-	To              *types.Script
-	Amount          uint64
-	Fee             uint64
-	UseIndexer      bool
-	group           []int
-	witnessArgs     *types.WitnessArgs
-	tx              *types.Transaction
-	fromBlockNumber uint64
+	From        *types.Script
+	To          *types.Script
+	Amount      uint64
+	Fee         uint64
+	group       []int
+	witnessArgs *types.WitnessArgs
+	tx          *types.Transaction
 }
 
-func NewPayment(from, to string, amount, fee uint64, fromBlockNumber uint64, useIndexer bool) (*Payment, error) {
+func NewPayment(from, to string, amount, fee uint64) (*Payment, error) {
 	fromAddress, err := address.Parse(from)
 	if err != nil {
 		return nil, fmt.Errorf("parse from address %s error: %v", from, err)
@@ -40,78 +38,15 @@ func NewPayment(from, to string, amount, fee uint64, fromBlockNumber uint64, use
 	}
 
 	return &Payment{
-		From:            fromAddress.Script,
-		To:              toAddress.Script,
-		Amount:          amount,
-		Fee:             fee,
-		fromBlockNumber: fromBlockNumber,
-		UseIndexer:      useIndexer,
+		From:   fromAddress.Script,
+		To:     toAddress.Script,
+		Amount: amount,
+		Fee:    fee,
 	}, nil
 }
 
 func (p *Payment) GenerateTx(client rpc.Client) (*types.Transaction, error) {
-	if p.UseIndexer {
-		return generateTxWithIndexer(client, p)
-	} else {
-		return generateTx(client, p)
-	}
-}
-
-func generateTx(client rpc.Client, p *Payment) (*types.Transaction, error) {
-	collector := utils.NewCellCollector(client, p.From, utils.NewCapacityCellProcessor(p.Amount+p.Fee), p.fromBlockNumber)
-
-	result, err := collector.Collect()
-	if err != nil {
-		return nil, fmt.Errorf("collect cell error: %v", err)
-	}
-
-	if result.Capacity < p.Amount+p.Fee {
-		return nil, fmt.Errorf("insufficient balance: %d", result.Capacity)
-	}
-
-	systemScripts, err := utils.NewSystemScripts(client)
-	if err != nil {
-		return nil, fmt.Errorf("load system script error: %v", err)
-	}
-
-	tx := transaction.NewSecp256k1SingleSigTx(systemScripts)
-	tx.Outputs = append(tx.Outputs, &types.CellOutput{
-		Capacity: p.Amount,
-		Lock:     p.To,
-	})
-	tx.OutputsData = [][]byte{{}}
-
-	if result.Capacity-p.Amount-p.Fee > 0 {
-		if result.Capacity-p.Amount-p.Fee >= 6100000000 {
-			tx.Outputs = append(tx.Outputs, &types.CellOutput{
-				Capacity: result.Capacity - p.Amount - p.Fee,
-				Lock:     p.From,
-			})
-			tx.OutputsData = [][]byte{{}, {}}
-		} else {
-			tx.Outputs[0].Capacity = result.Capacity - p.Fee
-		}
-	}
-	var inputs []*types.CellInput
-	for _, cell := range result.Cells {
-		input := &types.CellInput{
-			Since: 0,
-			PreviousOutput: &types.OutPoint{
-				TxHash: cell.OutPoint.TxHash,
-				Index:  cell.OutPoint.Index,
-			},
-		}
-		inputs = append(inputs, input)
-	}
-	group, witnessArgs, err := transaction.AddInputsForTransaction(tx, inputs)
-	if err != nil {
-		return nil, fmt.Errorf("add inputs to transaction error: %v", err)
-	}
-
-	p.group = group
-	p.witnessArgs = witnessArgs
-	p.tx = tx
-	return tx, err
+	return generateTxWithIndexer(client, p)
 }
 
 func generateTxWithIndexer(client rpc.Client, p *Payment) (*types.Transaction, error) {
