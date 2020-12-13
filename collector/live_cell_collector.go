e9f98f06a23dd888112dccacfc69ffe39abce2bc
shaojunda <shaojunda@gmail.com>
2020-12-13 20:35:02 +0800

    refactor: move live cell collector to cell_collector folder

---
+++ collector/live_cell_collector.go
@@ -0,0 +1,107 @@
+package collector
+
+import (
+	"context"
+	"github.com/nervosnetwork/ckb-sdk-go/indexer"
+	"github.com/nervosnetwork/ckb-sdk-go/rpc"
+	"github.com/nervosnetwork/ckb-sdk-go/types"
+	"github.com/pkg/errors"
+)
+
+type LiveCellCollectResult struct {
+	LiveCells []*indexer.LiveCell
+	Capacity  uint64
+	Options   map[string]interface{}
+}
+
+type CellCollectionIterator interface {
+	HasNext() bool
+	Next()
+	CurrentItem() (*indexer.LiveCell, error)
+	Iterator() (CellCollectionIterator, error)
+}
+
+type LiveCellCollector struct {
+	Client      rpc.Client
+	SearchKey   *indexer.SearchKey
+	SearchOrder indexer.SearchOrder
+	Limit       uint64
+	LastCursor  string
+	EmptyData   bool
+	TypeScript  *types.Script
+	result      []*indexer.LiveCell
+	itemIndex   int
+}
+
+func (c *LiveCellCollector) HasNext() bool {
+	return c.itemIndex < len(c.result) || c.LastCursor != ""
+}
+
+func (c *LiveCellCollector) Next() {
+	c.itemIndex++
+}
+
+func (c *LiveCellCollector) CurrentItem() (*indexer.LiveCell, error) {
+	if c.itemIndex >= len(c.result) {
+		if c.LastCursor != "" {
+			c.itemIndex = 0
+			c.result = []*indexer.LiveCell{}
+		} else {
+			return nil, errors.New("no such element")
+		}
+	}
+	return c.result[c.itemIndex], nil
+}
+
+func (c *LiveCellCollector) Iterator() (CellCollectionIterator, error) {
+	result, lastCursor, err := c.collect()
+	if err != nil {
+		return nil, err
+	}
+	c.result = result
+	c.LastCursor = lastCursor
+
+	return c, nil
+}
+
+func (c *LiveCellCollector) collect() ([]*indexer.LiveCell, string, error) {
+	if c.SearchKey == nil {
+		return nil, "", errors.New("missing SearchKey error")
+	}
+	if c.SearchOrder != indexer.SearchOrderAsc && c.SearchOrder != indexer.SearchOrderDesc {
+		return nil, "", errors.New("missing SearchOrder error")
+	}
+	var result []*indexer.LiveCell
+	var lastCursor string
+	liveCells, err := c.Client.GetCells(context.Background(), c.SearchKey, c.SearchOrder, c.Limit, c.LastCursor)
+	if err != nil {
+		return nil, "", err
+	}
+	for _, cell := range liveCells.Objects {
+		if c.TypeScript != nil {
+			if !c.TypeScript.Equals(cell.Output.Type) {
+				continue
+			}
+		} else {
+			if cell.Output.Type != nil {
+				continue
+			}
+		}
+		if c.EmptyData && len(cell.OutputData) > 0 {
+			continue
+		}
+		result = append(result, cell)
+	}
+	lastCursor = liveCells.LastCursor
+	return result, lastCursor, nil
+}
+
+func NewLiveCellCollector(client rpc.Client, searchKey *indexer.SearchKey, searchOrder indexer.SearchOrder, limit uint64, afterCursor string) *LiveCellCollector {
+	return &LiveCellCollector{
+		Client:      client,
+		SearchKey:   searchKey,
+		SearchOrder: searchOrder,
+		Limit:       limit,
+		LastCursor:  afterCursor,
+	}
+}
