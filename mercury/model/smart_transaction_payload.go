9ad61d3f69422c5fc58b15830942bec333294eb9
zhengjianhui <815168733@qq.com>
2021-09-09 01:43:16 +0800

    feat(mercury-rpc): build-smart-transfer-transaction support

---
+++ mercury/model/smart_transaction_payload.go
@@ -0,0 +1,66 @@
+package model
+
+import (
+	"github.com/nervosnetwork/ckb-sdk-go/mercury/model/common"
+	"math/big"
+)
+
+type SmartTransferPayload struct {
+	AssetInfo *common.AssetInfo
+	From      []string
+	To        []*SmartTo
+	Change    string
+	FeeRate   uint
+}
+
+type SmartTo struct {
+	Address string
+	Amount  *big.Int
+}
+
+type smartTransferPayloadBuilder struct {
+	AssetInfo *common.AssetInfo
+	From      []string
+	To        []*SmartTo
+	Change    string
+	FeeRate   uint
+}
+
+func (builder *smartTransferPayloadBuilder) AddAssetInfo(info *common.AssetInfo) {
+	builder.AssetInfo = info
+}
+
+func (builder *smartTransferPayloadBuilder) AddFrom(from string) {
+	builder.From = append(builder.From, from)
+}
+
+func (builder *smartTransferPayloadBuilder) AddSmartTo(address string, amount *big.Int) {
+	builder.To = append(builder.To, &SmartTo{address, amount})
+}
+
+func (builder *smartTransferPayloadBuilder) AddChange(change string) {
+	builder.Change = change
+}
+
+func (builder *smartTransferPayloadBuilder) AddFeeRate(feeRate uint) {
+	builder.FeeRate = feeRate
+}
+
+func (builder *smartTransferPayloadBuilder) Build() *SmartTransferPayload {
+	return &SmartTransferPayload{
+		builder.AssetInfo,
+		builder.From,
+		builder.To,
+		builder.Change,
+		builder.FeeRate,
+	}
+}
+
+func NewSmartTransferPayloadBuilder() *smartTransferPayloadBuilder {
+	// default fee rate
+	return &smartTransferPayloadBuilder{
+		From:    make([]string, 0),
+		To:      make([]*SmartTo, 0),
+		FeeRate: 1000,
+	}
+}
