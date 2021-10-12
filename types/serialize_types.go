20f68bd7955217592b7ebb78fcf4c499a87e19d1
zhengjianhui <815168733@qq.com>
2021-10-12 16:04:54 +0800

    fix(ckb-address): Fix the case that script type data1 is not taken into account during bech32m serialization and deserialization (#82)
    * fix(test): Skip use cases that require node access at ci (#81)
    
    * feat(ckb-rpc): Ckb v0.100.0 version hard fork modification
    
    * feat(bech32m): Bech32m support
    
    * release(tag): v0.100.0-beta.1
    
    * fix(ckb-address): Fix the case that script type data1 is not taken into account during bech32m serialization and deserialization

--- types/serialize_types.go
+++ types/serialize_types.go
@@ -186,3 +186,27 @@ func (w *WitnessArgs) Serialize() ([]byte, error) {
 
 	return SerializeTable([][]byte{l, i, o}), nil
 }
+
+func SerializeHashType(hashType ScriptHashType) (string, error) {
+	if HashTypeData == hashType {
+		return "00", nil
+	} else if HashTypeType == hashType {
+		return "01", nil
+	} else if HashTypeData1 == hashType {
+		return "02", nil
+	}
+
+	return "", errors.New("Invalid script hash_type: " + string(hashType))
+}
+
+func DeserializeHashType(hashType string) (ScriptHashType, error) {
+	if "00" == hashType {
+		return HashTypeData, nil
+	} else if "01" == hashType {
+		return HashTypeType, nil
+	} else if "02" == hashType {
+		return HashTypeData1, nil
+	}
+
+	return "", errors.New("Invalid script hash_type: " + hashType)
+}
