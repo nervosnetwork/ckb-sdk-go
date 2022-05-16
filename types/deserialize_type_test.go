791489da97e54cd662ff75ebf9f5fe5e09de13b5
fjchen7 <fjchen7@gmail.com>
2022-05-16 14:44:05 +0800

    Add method to deserialize `WitnessArgs`

---
+++ types/deserialize_type_test.go
@@ -0,0 +1,23 @@
+package types
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDeserializeWitnessArgs(t *testing.T) {
+	witnessArgsBytes := common.FromHex("0x2700000010000000180000002000000004000000123400000400000056780000030000009a0000")
+
+	witnessArgs, _ := DeserializeWitnessArgs(witnessArgsBytes)
+
+	assert.Equal(t, []byte{0x12, 0x34, 0x00, 0x00}, witnessArgs.Lock)
+	assert.Equal(t, []byte{0x56, 0x78, 0x00, 0x00}, witnessArgs.InputType)
+	assert.Equal(t, []byte{0x9a, 0x00, 0x00}, witnessArgs.OutputType)
+
+	witnessArgsBytes = common.FromHex("0x10000000100000001000000010000000")
+	witnessArgs, _ = DeserializeWitnessArgs(witnessArgsBytes)
+	assert.Nil(t, witnessArgs.Lock)
+	assert.Nil(t, witnessArgs.InputType)
+	assert.Nil(t, witnessArgs.OutputType)
+}
