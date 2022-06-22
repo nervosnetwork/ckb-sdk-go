15674041e25e4028e83f40a30d8bcb059d11115c
fjchen7 <fjchen7@gmail.com>
2022-06-22 12:36:42 +0800

    Add type LocalNode and RemoteNode

--- types/net.go
+++ types/net.go
@@ -12,6 +12,47 @@ type Node struct {
 	Version    string         `json:"version"`
 }
 
+type LocalNode struct {
+	Version     string               `json:"version"`
+	NodeId      string               `json:"node_id"`
+	Active      bool                 `json:"active"`
+	Addresses   []*NodeAddress       `json:"addresses"`
+	Protocols   []*LocalNodeProtocol `json:"protocols"`
+	Connections uint64               `json:"connections"`
+}
+
+type LocalNodeProtocol struct {
+	Id              uint64   `json:"id"`
+	Name            string   `json:"name"`
+	SupportVersions []string `json:"support_versions"`
+}
+
+type RemoteNode struct {
+	Version           string                `json:"version"`
+	NodeID            string                `json:"node_id"`
+	Addresses         []*NodeAddress        `json:"addresses"`
+	IsOutbound        bool                  `json:"is_outbound"`
+	ConnectedDuration uint64                `json:"connected_duration"`
+	LastPingDuration  uint64                `json:"last_ping_duration,omitempty"`
+	SyncState         *PeerSyncState        `json:"sync_state,omitempty"`
+	Protocols         []*RemoteNodeProtocol `json:"protocols"`
+}
+
+type RemoteNodeProtocol struct {
+	ID      uint64 `json:"id"`
+	Version string `json:"version"`
+}
+
+type PeerSyncState struct {
+	BestKnownHeaderHash    Hash   `json:"best_known_header_hash,omitempty"`
+	BestKnownHeaderNumber  uint64 `json:"best_known_header_number,omitempty"`
+	LastCommonHeaderHash   Hash   `json:"last_common_header_hash,omitempty"`
+	LastCommonHeaderNumber uint64 `json:"last_common_header_number,omitempty"`
+	UnknownHeaderListSize  uint64 `json:"unknown_header_list_size"`
+	InflightCount          uint64 `json:"inflight_count"`
+	CanFetchCount          uint64 `json:"can_fetch_count"`
+}
+
 type BannedAddress struct {
 	Address   string `json:"address"`
 	BanReason string `json:"ban_reason"`
