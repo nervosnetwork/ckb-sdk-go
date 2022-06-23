91b9a4d5e3bd2d3efe764cd295299a8871c09d2f
fjchen7 <fjchen7@gmail.com>
2022-06-23 13:11:12 +0800

    Replace common.OutPoint by types.OutPoint in get_spent_transaction_example.go

--- mercury/example/get_spent_transaction_example.go
+++ mercury/example/get_spent_transaction_example.go
@@ -5,16 +5,15 @@ import (
 	"fmt"
 	"github.com/nervosnetwork/ckb-sdk-go/mercury/example/constant"
 	"github.com/nervosnetwork/ckb-sdk-go/mercury/model"
-	"github.com/nervosnetwork/ckb-sdk-go/mercury/model/common"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
 	"testing"
 )
 
 func TestGetSpentTransactionView(t *testing.T) {
 	payload := &model.GetSpentTransactionPayload{
-		OutPoint: common.OutPoint{
-			types.HexToHash("0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece"),
-			01,
+		OutPoint: types.OutPoint{
+			TxHash: types.HexToHash("0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece"),
+			Index:  01,
 		},
 	}
 
@@ -32,9 +31,9 @@ func TestGetSpentTransactionView(t *testing.T) {
 
 func TestGetSpentTransactionInfo(t *testing.T) {
 	payload := &model.GetSpentTransactionPayload{
-		OutPoint: common.OutPoint{
-			types.HexToHash("0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece"),
-			01,
+		OutPoint: types.OutPoint{
+			TxHash: types.HexToHash("0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece"),
+			Index:  01,
 		},
 	}
 
