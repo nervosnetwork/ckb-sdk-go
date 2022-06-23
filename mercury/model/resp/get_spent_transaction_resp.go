3acc4960a7b1912c3787ae028d4860b6a5835b70
fjchen7 <fjchen7@gmail.com>
2022-06-23 09:04:36 +0800

    Refactor query transactions response

--- mercury/model/resp/get_spent_transaction_resp.go
+++ mercury/model/resp/get_spent_transaction_resp.go
@@ -3,18 +3,18 @@ package resp
 import "github.com/nervosnetwork/ckb-sdk-go/mercury/model/common"
 
 type TransactionInfoWrapper struct {
-	Type  TransactionType `json:"type"`
-	Value TransactionInfo `json:"value"`
+	Type  TransactionType  `json:"type"`
+	Value *TransactionInfo `json:"value"`
 }
 
 type TransactionViewWrapper struct {
-	Type  TransactionType                  `json:"type"`
-	Value common.TransactionWithRichStatus `json:"value`
+	Type  TransactionType                   `json:"type"`
+	Value *common.TransactionWithRichStatus `json:"value"`
 }
 
 type TransactionType string
 
 const (
-	TransactionTransactionView TransactionType = "TransactionView"
-	TransactionTransactionInfo                 = "TransactionInfo"
+	TransactionTransactionView TransactionType = "TransactionWithRichStatus"
+	TransactionTransactionInfo TransactionType = "TransactionInfo"
 )
