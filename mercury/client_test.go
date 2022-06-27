c81f25c6f098c219b62bd06f087e57b0c3863fc1
fjchen7 <fjchen7@gmail.com>
2022-06-27 16:47:11 +0800

    Check error

--- mercury/client_test.go
+++ mercury/client_test.go
@@ -13,8 +13,10 @@ import (
 var c, _ = Dial("https://mercury-testnet.ckbapp.dev/0.4")
 
 func TestBuildAdjustAccountTransaction(t *testing.T) {
-	item, _ := req.NewIdentityItemByPublicKeyHash("0xb0f8a32e7f9e8f3ab3a641f6eb02fcdb921d5589")
-	from, _ := req.NewIdentityItemByPublicKeyHash("0x202647fecc5b9d8cbdb4ae7167e40f5ab1e4baaf")
+	item, err := req.NewIdentityItemByPublicKeyHash("0xb0f8a32e7f9e8f3ab3a641f6eb02fcdb921d5589")
+	checkError(t, err)
+	from, err := req.NewIdentityItemByPublicKeyHash("0x202647fecc5b9d8cbdb4ae7167e40f5ab1e4baaf")
+	checkError(t, err)
 	payload := &model.BuildAdjustAccountPayload{
 		Item:          item,
 		From:          []*req.Item{from},
@@ -23,22 +25,24 @@ func TestBuildAdjustAccountTransaction(t *testing.T) {
 		ExtraCKB:      20000000000,
 		FeeRate:       1000,
 	}
-	tx, _ := c.BuildAdjustAccountTransaction(payload)
-
-	assert.NotNil(t, tx.TxView)
-	assert.NotNil(t, tx.ScriptGroups)
-	assert.True(t, len(tx.ScriptGroups) >= 1)
+	resp, err := c.BuildAdjustAccountTransaction(payload)
+	checkError(t, err)
+	assert.NotNil(t, resp.TxView)
+	assert.NotNil(t, resp.ScriptGroups)
+	assert.True(t, len(resp.ScriptGroups) >= 1)
 }
 
 func TestGetBalance(t *testing.T) {
-	item, _ := req.NewIdentityItemByPublicKeyHash("0x839f1806e85b40c13d3c73866045476cc9a8c214")
+	item, err := req.NewIdentityItemByPublicKeyHash("0x839f1806e85b40c13d3c73866045476cc9a8c214")
+	checkError(t, err)
 	payload := &model.GetBalancePayload{
 		Item: item,
 		AssetInfos: []*common.AssetInfo{
 			common.NewUdtAsset1(types.HexToHash("0xf21e7350fa9518ed3cbb008e0e8c941d7e01a12181931d5608aa366ee22228bd"))},
 		TipBlockNumber: 0,
 	}
-	resp, _ := c.GetBalance(payload)
+	resp, err := c.GetBalance(payload)
+	checkError(t, err)
 	assert.Equal(t, 2, len(resp.Balances))
 	assert.Equal(t, types.HexToHash("0xf21e7350fa9518ed3cbb008e0e8c941d7e01a12181931d5608aa366ee22228bd"),
 		resp.Balances[0].AssetInfo.UdtHash)
@@ -46,7 +50,8 @@ func TestGetBalance(t *testing.T) {
 
 func TestBuildSudtIssueTransaction(t *testing.T) {
 	address := "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg958atl2zdh8jn3ch8lc72nt0cf864ecqdxm9zf"
-	item, _ := req.NewIdentityItemByAddress(address)
+	item, err := req.NewIdentityItemByAddress(address)
+	checkError(t, err)
 	payload := &model.BuildSudtIssueTransactionPayload{
 		Owner: address,
 		From:  []*req.Item{item},
@@ -59,7 +64,8 @@ func TestBuildSudtIssueTransaction(t *testing.T) {
 		OutputCapacityProvider: model.OutputCapacityProviderFrom,
 		FeeRate:                1000,
 	}
-	resp, _ := c.BuildSudtIssueTransaction(payload)
+	resp, err := c.BuildSudtIssueTransaction(payload)
+	checkError(t, err)
 	assert.NotNil(t, resp.TxView)
 	assert.NotNil(t, resp.ScriptGroups)
 }
