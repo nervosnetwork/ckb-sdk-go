4d49ec6010d701028ea44499dcb713badae533b5
fjchen7 <fjchen7@gmail.com>
2022-07-06 17:46:09 +0800

    Implement `AnyCanPaySigner`

---
+++ transaction/signer/any_can_pay.go
@@ -0,0 +1,27 @@
+package signer
+
+import (
+	"github.com/nervosnetwork/ckb-sdk-go/crypto/secp256k1"
+	"github.com/nervosnetwork/ckb-sdk-go/transaction"
+	"github.com/nervosnetwork/ckb-sdk-go/types"
+)
+
+type AnyCanPaySigner struct {
+}
+
+func (s AnyCanPaySigner) SignTransaction(transaction *types.Transaction, group *transaction.ScriptGroup, ctx *transaction.Context) (bool, error) {
+	key := ctx.Key
+	matched, err := IsAnyCanPayMatched(key, group.Script.Args)
+	if err != nil {
+		return false, err
+	}
+	if matched {
+		return SingleSignTransaction(transaction, group, key)
+	} else {
+		return false, nil
+	}
+}
+
+func IsAnyCanPayMatched(key *secp256k1.Secp256k1Key, scriptArgs []byte) (bool, error) {
+	return IsSingleSigMatched(key, scriptArgs[:20])
+}
