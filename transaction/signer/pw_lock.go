33eb59a5f49401aacd4d269cf5953ed98adb9ff9
fjchen7 <fjchen7@gmail.com>
2022-07-07 11:08:23 +0800

    Implement function SignTransaction for PWLockSigner

--- transaction/signer/pw_lock.go
+++ transaction/signer/pw_lock.go
@@ -6,6 +6,7 @@ import (
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/secp256k1"
 	"github.com/nervosnetwork/ckb-sdk-go/transaction"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
+	"strconv"
 )
 
 type PWLockSigner struct {
@@ -13,16 +14,50 @@ type PWLockSigner struct {
 
 func (s PWLockSigner) SignTransaction(transaction *types.Transaction, group *transaction.ScriptGroup, ctx *transaction.Context) (bool, error) {
 	key := ctx.Key
-	matched, err := IsSingleSigMatched(key, group.Script.Args)
-	if err != nil {
-		return false, err
-	}
+	matched := IsPWLockMatched(key, group.Script.Args)
 	if matched {
-		// TODO
+		return PWLockSignTransaction(transaction, group, key)
 		return false, nil
 	} else {
 		return false, nil
 	}
+}
+
+func PWLockSignTransaction(tx *types.Transaction, group *transaction.ScriptGroup, key *secp256k1.Secp256k1Key) (bool, error) {
+	txHash, err := tx.ComputeHash()
+	if err != nil {
+		return false, err
+	}
+	data := txHash.Bytes()
+	for _, v := range group.InputIndices {
+		witness := tx.Witnesses[v]
+		data = append(data, types.SerializeUint64(uint64(len(witness)))...)
+		data = append(data, witness...)
+	}
+	for i := len(tx.Inputs); i < len(tx.Witnesses); i++ {
+		witness := tx.Witnesses[i]
+		data = append(data, types.SerializeUint64(uint64(len(witness)))...)
+		data = append(data, witness...)
+	}
+	message := crypto.Keccak256(data)
+	prefix := []byte("\u0019Ethereum Signed Message:\n" + strconv.Itoa(len(message)))
+	message = append(prefix, message...)
+	message = crypto.Keccak256(message)
+	signature, err := key.Sign(message)
+	if err != nil {
+		return false, err
+	}
+	i := group.InputIndices[0]
+	w := tx.Witnesses[i]
+	witnessArgs, err := types.DeserializeWitnessArgs(w)
+	if err != nil {
+		return false, err
+	}
+	witnessArgs.Lock = signature
+	if tx.Witnesses[i], err = witnessArgs.Serialize(); err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 func IsPWLockMatched(key *secp256k1.Secp256k1Key, scriptArgs []byte) bool {
