8692782d2d938a75e639432ffb5b9c63b246b49e
fjchen7 <fjchen7@gmail.com>
2022-07-15 13:34:05 +0800

    Add `GenerateSecp256k1Blake160MultisigScript`

--- address/address_tools.go
+++ address/address_tools.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/blake2b"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/secp256k1"
+	"github.com/nervosnetwork/ckb-sdk-go/transaction/signer"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
 	"github.com/nervosnetwork/ckb-sdk-go/utils"
 )
@@ -57,4 +58,54 @@ func isSecp256k1Lock(parsedSenderAddr *Address, systemScripts *utils.SystemScrip
 	return parsedSenderAddr.Script.CodeHash == systemScripts.SecpSingleSigCell.CellHash &&
 		parsedSenderAddr.Script.HashType == systemScripts.SecpSingleSigCell.HashType &&
 		len(parsedSenderAddr.Script.Args) == 20
+}
+
+// GenerateSecp256k1Blake160MultisigScript generate scep256k1 multisig script.
+// It can accept public key (in compressed format, 33 bytes each) array or public key hash (20 bytes) array, and
+// return error if giving none of them.
+func GenerateSecp256k1Blake160MultisigScript(requireN, threshold int, publicKeysOrHashes [][]byte) (*types.Script, []byte, error) {
+	multisigScript := signer.MultisigScript{
+		Version:    0,
+		FirstN:     byte(requireN),
+		Threshold:  byte(threshold),
+		KeysHashes: [][20]byte{},
+	}
+
+	isPublicKeyHash := len(publicKeysOrHashes[0]) == 20
+	if isPublicKeyHash {
+		for _, publicKeyHash := range publicKeysOrHashes {
+			if len(publicKeyHash) != 20 {
+				return nil, nil, errors.New("public key hash length must be 20 bytes")
+			}
+			if err := multisigScript.AddKeyHashBySlice(publicKeyHash); err != nil {
+				return nil, nil, err
+			}
+		}
+	} else {
+		for _, publicKey := range publicKeysOrHashes {
+			if len(publicKey) != 33 {
+				return nil, nil, errors.New("public key (compressed) length must be 33 bytes")
+			}
+			publicKeyHash, err := blake2b.Blake160(publicKey)
+			if err != nil {
+				return nil, nil, err
+			}
+			if err := multisigScript.AddKeyHashBySlice(publicKeyHash); err != nil {
+				return nil, nil, err
+			}
+		}
+	}
+
+	args, err := multisigScript.ComputeHash()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// secp256k1_blake160_multisig_all share the same code hash in network main and test
+	codeHash := types.GetCodeHash(types.BuiltinScriptSecp256k1Blake160MultisigAll, types.NetworkTest)
+	return &types.Script{
+		CodeHash: codeHash,
+		HashType: types.HashTypeType,
+		Args:     args,
+	}, multisigScript.Encode(), nil
 }
--- address/address_tools_test.go
+++ address/address_tools_test.go
@@ -1,7 +1,9 @@
 package address
 
 import (
+	"encoding/hex"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/nervosnetwork/ckb-sdk-go/crypto/blake2b"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/secp256k1"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
 	"github.com/stretchr/testify/assert"
@@ -38,4 +40,75 @@ func TestGenerateScriptSecp256K1Blake160SignhashAll(t *testing.T) {
 	// test uncompressed public key
 	_, err = GenerateScriptSecp256K1Blake160SignhashAllByPublicKey("0x044a501efd328e062c8675f2365970728c859c592beeefd6be8ead3d901330bc01d1868c7dabbf50e52ca7311e1263f917a8ced1d033e82dc2a68bed69397382f4")
 	assert.NotNil(t, err)
+}
+
+func TestGenerateSecp256k1MultisigScript(t *testing.T) {
+	var publicKeys [][]byte
+
+	key, err := hex.DecodeString("032edb83018b57ddeb9bcc7287c5cc5da57e6e0289d31c9e98cb361e88678d6288")
+	if err != nil {
+		t.Error(t, err)
+	}
+	publicKeys = append(publicKeys, key)
+
+	key, err = hex.DecodeString("033aeb3fdbfaac72e9e34c55884a401ee87115302c146dd9e314677d826375dc8f")
+	if err != nil {
+		t.Error(t, err)
+	}
+	publicKeys = append(publicKeys, key)
+
+	key, err = hex.DecodeString("029a685b8206550ea1b600e347f18fd6115bffe582089d3567bec7eba57d04df01")
+	if err != nil {
+		t.Error(t, err)
+	}
+	publicKeys = append(publicKeys, key)
+
+	script, _, err := GenerateSecp256k1Blake160MultisigScript(0, 2, publicKeys)
+	if err != nil {
+		t.Error(t, err)
+	}
+
+	address := &Address{
+		Script:  script,
+		Network: types.NetworkTest,
+	}
+	encoded, err := address.Encode()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, "ckt1qpw9q60tppt7l3j7r09qcp7lxnp3vcanvgha8pmvsa3jplykxn32sq0sfnkgf0ph76pkzwld9ujzex4pkeuwnlsdc5tqu", encoded)
+}
+
+func TestGenerateSecp256k1MultisigScriptByHash(t *testing.T) {
+	publicKeysHex := []string{
+		"032edb83018b57ddeb9bcc7287c5cc5da57e6e0289d31c9e98cb361e88678d6288",
+		"033aeb3fdbfaac72e9e34c55884a401ee87115302c146dd9e314677d826375dc8f",
+		"029a685b8206550ea1b600e347f18fd6115bffe582089d3567bec7eba57d04df01",
+	}
+	var publicKeysHash [][]byte
+	for _, publicKeyHex := range publicKeysHex {
+		key, err := hex.DecodeString(publicKeyHex)
+		if err != nil {
+			t.Error(t, err)
+		}
+		hash, err := blake2b.Blake160(key)
+		if err != nil {
+			t.Error(t, err)
+		}
+		publicKeysHash = append(publicKeysHash, hash)
+	}
+	script, _, err := GenerateSecp256k1Blake160MultisigScript(0, 2, publicKeysHash)
+	if err != nil {
+		t.Error(t, err)
+	}
+
+	address := &Address{
+		Script:  script,
+		Network: types.NetworkTest,
+	}
+	encoded, err := address.Encode()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, "ckt1qpw9q60tppt7l3j7r09qcp7lxnp3vcanvgha8pmvsa3jplykxn32sq0sfnkgf0ph76pkzwld9ujzex4pkeuwnlsdc5tqu", encoded)
 }
