e28d53a466b7ab3b88fef229586cc1d353bca939
fjchen7 <fjchen7@gmail.com>
2022-07-21 15:49:41 +0800

    Implement `Secp256k1Blake160SighashAllScriptHandler`

---
+++ collector/handler_ckb.go
@@ -0,0 +1,55 @@
+package collector
+
+import (
+	"github.com/nervosnetwork/ckb-sdk-go/transaction"
+	"github.com/nervosnetwork/ckb-sdk-go/types"
+	"reflect"
+)
+
+type Secp256k1Blake160SighashAllScriptHandler struct {
+	cellDep *types.CellDep
+}
+
+func NewSecp256k1Blake160SighashAllScriptHandler(network types.Network) *Secp256k1Blake160SighashAllScriptHandler {
+	var txHash types.Hash
+	if network == types.NetworkMain {
+		txHash = types.HexToHash("0x71a7ba8fc96349fea0ed3a5c47992e3b4084b031a42264a018e0072e8172e46c")
+	} else if network == types.NetworkTest {
+		txHash = types.HexToHash("0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37")
+	} else {
+		return nil
+	}
+
+	return &Secp256k1Blake160SighashAllScriptHandler{
+		cellDep: &types.CellDep{
+
+			OutPoint: &types.OutPoint{
+				TxHash: txHash,
+				Index:  0,
+			},
+			DepType: types.DepTypeDepGroup,
+		},
+	}
+}
+
+func (r *Secp256k1Blake160SighashAllScriptHandler) isMatched(script *types.Script) bool {
+	if script == nil {
+		return false
+	}
+
+	codeHash := types.GetCodeHash(types.BuiltinScriptSecp256k1Blake160SighashAll, types.NetworkMain)
+	return reflect.DeepEqual(script.CodeHash, codeHash)
+}
+
+func (r *Secp256k1Blake160SighashAllScriptHandler) BuildTransaction(builder TransactionBuilder, group *transaction.ScriptGroup, context interface{}) (bool, error) {
+	if group == nil || !r.isMatched(group.Script) {
+		return false, nil
+	}
+	index := group.InputIndices[0]
+	lock := [65]byte{}
+	if err := builder.SetWitness(uint(index), types.WitnessTypeLock, lock[:]); err != nil {
+		return false, err
+	}
+	builder.AddCellDep(r.cellDep)
+	return true, nil
+}
