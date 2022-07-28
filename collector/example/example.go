122bba10c98027694c4609ebdabef4fb78962b5e
fjchen7 <fjchen7@gmail.com>
2022-07-28 16:13:47 +0800

    Add `ClaimDaoExample`

--- collector/example/example.go
+++ collector/example/example.go
@@ -234,3 +234,55 @@ func WithdrawDaoExample() error {
 	fmt.Println("transaction hash: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
+
+func ClaimDaoExample() error {
+	sender := "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r"
+	withdrawOutPoint := &types.OutPoint{
+		TxHash: types.HexToHash("0xdb5b3ad48af332df51d0c87fcf7a180d7f8e284b743c08e4338cdd36b0a4b456"),
+		Index:  0,
+	}
+
+	network := types.NetworkTest
+	indexerClient, err := indexer.Dial("https://testnet.ckb.dev/indexer")
+	if err != nil {
+		return err
+	}
+	ckbClient, err := rpc.Dial("https://testnet.ckb.dev")
+	if err != nil {
+		return err
+	}
+
+	iterator, err := collector.NewLiveCellIteratorFromAddress(indexerClient, sender)
+	if err != nil {
+		return err
+	}
+
+	builder, err := builder2.NewDaoTransactionBuilder(network, iterator, withdrawOutPoint, ckbClient)
+	if err != nil {
+		return err
+	}
+	builder.FeeRate = 1000
+	builder.AddChangeOutputByAddress(sender)
+
+	claimInfo, err := handler.NewClaimInfo(ckbClient, withdrawOutPoint)
+	if err != nil {
+		return err
+	}
+	txWithGroups, err := builder.Build(claimInfo)
+	if err != nil {
+		return err
+	}
+
+	txSigner := signer.GetTransactionSignerInstance(network)
+	_, err = txSigner.SignTransactionByPrivateKeys(txWithGroups, "0x6c9ed03816e3111e49384b8d180174ad08e29feb1393ea1b51cef1c505d4e36a")
+	if err != nil {
+		return err
+	}
+
+	hash, err := ckbClient.SendTransaction(context.Background(), txWithGroups.TxView)
+	if err != nil {
+		return err
+	}
+	fmt.Println("transaction hash: " + hexutil.Encode(hash.Bytes()))
+	return nil
+}
