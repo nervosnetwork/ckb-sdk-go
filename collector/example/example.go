0957333da9830b3082d9f0a88f95639ed15d0b74
fjchen7 <fjchen7@gmail.com>
2022-07-28 15:01:48 +0800

    Add `DepositDaoExample`

--- collector/example/example.go
+++ collector/example/example.go
@@ -138,3 +138,42 @@ func SendSudtExample() error {
 	fmt.Println("transaction hash: " + hexutil.Encode(hash.Bytes()))
 	return nil
 }
+
+func DepositDaoExample() error {
+	sender := "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq2qf8keemy2p5uu0g0gn8cd4ju23s5269qk8rg4r"
+	network := types.NetworkTest
+	indexerClient, err := indexer.Dial("https://testnet.ckb.dev/indexer")
+	if err != nil {
+		return err
+	}
+	iterator, err := collector.NewLiveCellIteratorFromAddress(indexerClient, sender)
+	if err != nil {
+		return err
+	}
+
+	builder := builder2.NewCkbTransactionBuilder(network, iterator)
+	builder.FeeRate = 1000
+	if err := builder.AddDaoDepositOutputByAddress(sender, 50100000000); err != nil {
+		return err
+	}
+
+	builder.AddChangeOutputByAddress(sender)
+	txWithGroups, err := builder.Build()
+	if err != nil {
+		return err
+	}
+
+	txSigner := signer.GetTransactionSignerInstance(network)
+	_, err = txSigner.SignTransactionByPrivateKeys(txWithGroups, "0x6c9ed03816e3111e49384b8d180174ad08e29feb1393ea1b51cef1c505d4e36a")
+	if err != nil {
+		return err
+	}
+
+	ckbClient, err := rpc.Dial("https://testnet.ckb.dev")
+	hash, err := ckbClient.SendTransaction(context.Background(), txWithGroups.TxView)
+	if err != nil {
+		return err
+	}
+	fmt.Println("transaction hash: " + hexutil.Encode(hash.Bytes()))
+	return nil
+}
