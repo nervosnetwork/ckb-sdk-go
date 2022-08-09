697dea3d5542976e5fe6de28a18b9223e2a4493f
fjchen7 <fjchen7@gmail.com>
2022-08-09 14:23:35 +0800

    Remove method `CkbWithDecimalToShannon`

--- utils/amount/capacity.go
+++ utils/amount/capacity.go
@@ -2,10 +2,6 @@ package amount
 
 import "math"
 
-func CkbWithDecimalToShannon(amount float64) uint64 {
-	return uint64(amount * float64(exponent(10, 8)))
-}
-
 func CkbToShannon(amount uint64) uint64 {
 	return amount * exponent(10, 8)
 }
--- utils/amount/capacity_test.go
+++ utils/amount/capacity_test.go
@@ -7,5 +7,4 @@ import (
 
 func TestCkbToShannon(t *testing.T) {
 	assert.Equal(t, uint64(234300000000), CkbToShannon(2343))
-	assert.Equal(t, uint64(2560000), CkbWithDecimalToShannon(0.0256))
 }
