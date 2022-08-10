3971aa9deb7263ba4b7995d239558bef960e3c13
fjchen7 <fjchen7@gmail.com>
2022-08-10 17:58:23 +0800

    Rewrite function `SerializeUint64`

--- types/serialize_primitive.go
+++ types/serialize_primitive.go
@@ -1,21 +1,11 @@
 package types
 
-import (
-	"encoding/binary"
-)
-
 const u32Size uint = 4
 
-func SerializeUint(n uint) []byte {
-	b := make([]byte, u32Size)
-	binary.LittleEndian.PutUint32(b, uint32(n))
-
-	return b
+func SerializeUint32(n uint32) []byte {
+	return PackUint32(n).AsSlice()
 }
 
 func SerializeUint64(n uint64) []byte {
-	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, n)
-
-	return b
+	return PackUint64(n).AsSlice()
 }
