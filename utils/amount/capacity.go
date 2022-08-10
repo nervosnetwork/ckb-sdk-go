8e8670bf6d1e3c83de525ce22521e65fa201ea88
fjchen7 <fjchen7@gmail.com>
2022-08-10 10:19:26 +0800

    Remove additional `exponent` function

--- utils/amount/capacity.go
+++ utils/amount/capacity.go
@@ -1,11 +1,5 @@
 package amount
 
-import "math"
-
 func CkbToShannon(amount uint64) uint64 {
-	return amount * exponent(10, 8)
-}
-
-func exponent(a, n uint64) uint64 {
-	return uint64(math.Pow(float64(a), float64(n)))
+	return amount * uint64(100000000)
 }
