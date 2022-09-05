9cf034b405a534416571a418327276b480f7bda7
fjchen7 <fjchen7@gmail.com>
2022-09-05 15:18:30 +0800

    Remove unused address tool methods

--- address/address_tools.go
+++ address/address_tools.go
@@ -2,13 +2,11 @@ package address
 
 import (
 	"errors"
-	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/blake2b"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/secp256k1"
 	"github.com/nervosnetwork/ckb-sdk-go/transaction/signer"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
-	"github.com/nervosnetwork/ckb-sdk-go/utils"
 )
 
 func GenerateScriptSecp256K1Blake160SignhashAll(key *secp256k1.Secp256k1Key) *types.Script {
@@ -33,31 +31,6 @@ func GenerateScriptSecp256K1Blake160SignhashAllByPublicKey(pubKey string) (*type
 		HashType: types.HashTypeType,
 		Args:     args,
 	}, nil
-}
-
-func GenerateAddressSecp256K1Blake160SignhashAll(key *secp256k1.Secp256k1Key, network types.Network) *Address {
-	script := GenerateScriptSecp256K1Blake160SignhashAll(key)
-	return &Address{
-		Script:  script,
-		Network: network,
-	}
-}
-
-func ValidateChequeAddress(addr string, systemScripts *utils.SystemScripts) (*Address, error) {
-	address, err := Decode(addr)
-	if err != nil {
-		return nil, err
-	}
-	if isSecp256k1Lock(address, systemScripts) {
-		return address, nil
-	}
-	return nil, errors.New(fmt.Sprintf("address %s is not an SECP256K1 short format address", addr))
-}
-
-func isSecp256k1Lock(parsedSenderAddr *Address, systemScripts *utils.SystemScripts) bool {
-	return parsedSenderAddr.Script.CodeHash == systemScripts.SecpSingleSigCell.CodeHash &&
-		parsedSenderAddr.Script.HashType == systemScripts.SecpSingleSigCell.HashType &&
-		len(parsedSenderAddr.Script.Args) == 20
 }
 
 // GenerateSecp256k1Blake160MultisigScript generate scep256k1 multisig script.
