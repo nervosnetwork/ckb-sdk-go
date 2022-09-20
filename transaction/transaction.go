431f0f6ad05805227bb123cdfb20e6d431cf7ed0
fjchen7 <fjchen7@gmail.com>
2022-09-20 17:19:20 +0800

    Rewrite function `SignTransaction`

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -2,7 +2,7 @@ package transaction
 
 import (
 	"encoding/binary"
-	"errors"
+	"fmt"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto"
 	"github.com/nervosnetwork/ckb-sdk-go/crypto/blake2b"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
@@ -22,76 +22,46 @@ func NewEmptyWitnessArg(LockScriptLength uint) *types.WitnessArgs {
 	}
 }
 
-func AddInputsForTransaction(transaction *types.Transaction, inputs []*types.CellInput, signatureLengthInBytes uint) ([]int, *types.WitnessArgs, error) {
-	if len(inputs) == 0 {
-		return nil, nil, errors.New("input cells empty")
+// SignTransaction signs transaction with index group and witness placeholder in secp256k1_blake160_sighash_all way
+func SignTransaction(tx *types.Transaction, group []int, witnessPlaceholder []byte, key crypto.Key) ([]byte, error) {
+	inputsLen := len(tx.Inputs)
+	for i := 0; i < len(group); i++ {
+		if i > 0 && group[i] <= group[i-1] {
+			return nil, fmt.Errorf("group index is not in ascending order")
+		}
+		if group[i] > inputsLen {
+			return nil, fmt.Errorf("group index %d is greater than input bytesLen %d", group[i], inputsLen)
+		}
 	}
-	group := make([]int, len(inputs))
-	start := len(transaction.Inputs)
-	for i := 0; i < len(inputs); i++ {
-		input := inputs[i]
-		transaction.Inputs = append(transaction.Inputs, input)
-		transaction.Witnesses = append(transaction.Witnesses, []byte{})
-		group[i] = start + i
-	}
-	emptyWitnessArgs := NewEmptyWitnessArg(signatureLengthInBytes)
-	emptyWitnessArgsBytes := emptyWitnessArgs.Serialize()
-	transaction.Witnesses[start] = emptyWitnessArgsBytes
-	return group, emptyWitnessArgs, nil
-}
+	txHash := tx.ComputeHash()
+	msg := txHash.Bytes()
+	bytesLen := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytesLen, uint64(len(witnessPlaceholder)))
+	msg = append(msg, bytesLen...)
+	msg = append(msg, witnessPlaceholder...)
 
-// group is an array, which content is the index of input after grouping
-//func SingleSignTransaction(transaction *types.Transaction, group []int, witnessArgs *types.WitnessArgs, key crypto.Key) error {
-//	data := witnessArgs.Serialize()
-//	length := make([]byte, 8)
-//	binary.LittleEndian.PutUint64(length, uint64(len(data)))
-//
-//	hash, err := transaction.ComputeHash()
-//	if err != nil {
-//		return err
-//	}
-//
-//	message := append(hash.Bytes(), length...)
-//	message = append(message, data...)
-//	// hash the other witnesses in the group
-//	if len(group) > 1 {
-//		for i := 1; i < len(group); i++ {
-//			data = transaction.Witnesses[group[i]]
-//			length := make([]byte, 8)
-//			binary.LittleEndian.PutUint64(length, uint64(len(data)))
-//			message = append(message, length...)
-//			message = append(message, data...)
-//		}
-//	}
-//	// hash witnesses which do not in any input group
-//	for _, witness := range transaction.Witnesses[len(transaction.Inputs):] {
-//		length := make([]byte, 8)
-//		binary.LittleEndian.PutUint64(length, uint64(len(witness)))
-//		message = append(message, length...)
-//		message = append(message, witness...)
-//	}
-//
-//	message, err = blake2b.Blake256(message)
-//	if err != nil {
-//		return err
-//	}
-//
-//	signed, err := key.Sign(message)
-//	if err != nil {
-//		return err
-//	}
-//
-//	wa := &types.WitnessArgs{
-//		Lock:       signed,
-//		InputType:  witnessArgs.InputType,
-//		OutputType: witnessArgs.OutputType,
-//	}
-//	wab := wa.Serialize()
-//
-//	transaction.Witnesses[group[0]] = wab
-//
-//	return nil
-//}
+	var indexes []int
+	for i := 1; i < len(group); i++ {
+		indexes = append(indexes, group[i])
+	}
+	for i := inputsLen; i < len(tx.Witnesses); i++ {
+		indexes = append(indexes, i)
+	}
+	for _, i := range indexes {
+		bytes := tx.Witnesses[i]
+		bytesLen := make([]byte, 8)
+		binary.LittleEndian.PutUint64(bytesLen, uint64(len(bytes)))
+		msg = append(msg, bytesLen...)
+		msg = append(msg, bytes...)
+	}
+
+	msgHash := blake2b.Blake256(msg)
+	signature, err := key.Sign(msgHash)
+	if err != nil {
+		return nil, err
+	}
+	return signature, nil
+}
 
 func MsgFromTxForMultiSig(transaction *types.Transaction, group []int, multisigScript []byte) ([]byte, error) {
 	var witnessArgs *types.WitnessArgs
